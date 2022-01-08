73e7d9c66925a21696debec90c3f9187d72c9e7b
spaceCrazy <74096278+riddhijit2702@users.noreply.github.com>
2022-01-08 12:06:27 +0530

    Update class.py

--- class.py
+++ class.py
@@ -3,6 +3,6 @@ import plotly_express as px
 
 df = pd.read_csv("cc.csv")
 
-fig = px.line(df, x="Date", y="No. of cases everyday", color="Country", title='Covid Report')
+fig = px.scatter(df, x="Date", y="Cases", color="Country", title='Covid Report')
 
 fig.show()
